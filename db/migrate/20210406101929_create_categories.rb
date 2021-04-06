2a92ec7f80d2067dd23c2ee78524a6f29de51889
kokoriru <ogihara.yuka.b@gmail.com>
2021-04-07 03:09:07 +0900

    categories テーブルにて parent_categories テーブルとの関連をDB管理するために各種 migration ファイル見直し

--- db/migrate/20210406101929_create_categories.rb
+++ db/migrate/20210406101929_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
     create_table :categories do |t|
       t.string :name
       t.integer :sequence
+      t.belongs_to :parent, foreign_key: { to_table: :parent_categories }
 
       t.timestamps
     end
