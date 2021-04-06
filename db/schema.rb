f7d1d386532df2c2d39107259e4e183a0e045fdf
kokoriru <ogihara.yuka.b@gmail.com>
2021-04-06 20:13:39 +0900

    ProductCategory 中間モデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_101929) do
+ActiveRecord::Schema.define(version: 2021_04_06_111212) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "sequence"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -34,4 +43,6 @@ ActiveRecord::Schema.define(version: 2021_04_06_101929) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
