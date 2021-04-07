326469c4db2b687e6ce45a01421184975920186b
kokoriru <ogihara.yuka.b@gmail.com>
2021-04-07 10:18:49 +0900

    Wish List 新規作成ページ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,29 @@ class ProductsController < ApplicationController
 
   def show; end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path(@product)
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(
+      :manufacturer, :name, :status, :made_to_order,
+      :started_at, :ended_at, :reserved_at, :scheduled_arrival, :url, :note
+    )
   end
 end
