e1014f2c4f814d1d24e3776cae1638781aafb365
kokoriru <ogihara.yuka.b@gmail.com>
2021-04-07 10:16:42 +0900

    enum: status を Product モデルに追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+
+  enum status: %w[未定 購入予定 購入済 購入しない]
 end
